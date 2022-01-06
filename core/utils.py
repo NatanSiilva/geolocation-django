79e055ffd51e7c99624fcd28b6dd2a432d64142e
NatanSiilva <contato@natandev.com.br>
2022-01-06 14:48:50 -0300

    first commit

---
+++ core/utils.py
@@ -0,0 +1,48 @@
+import requests
+from random import randint
+from django.conf import settings
+from django.contrib.gis.geoip2 import GeoIP2
+from django.contrib.gis.geoip2 import geoip2
+
+YELP_SEARCH_ENDPOINT = "https://api.yelp.com/v3/businesses/search"
+
+
+def yelp_search(keyword=None, location=None):
+    headers = {"Authorization": "Bearer " + settings.YELP_API_KEY}
+
+    if keyword and location:
+        params = {
+            "term": keyword,
+            "location": location,
+        }
+    else:
+        params = {
+            "term": "Pizzaria",
+            "location": "São Paulo",
+        }
+
+    res = requests.get(YELP_SEARCH_ENDPOINT, headers=headers, params=params)
+
+    return res.json()
+
+
+def get_client_data():
+    g = GeoIP2()
+    ip = get_random_ip()
+
+    try:
+        return g.city(ip)
+    except geoip2.errors.AddressNotFoundError:
+        return None
+
+
+def get_random_ip():
+    # first = randint(1, 254)
+    # second = randint(1, 254)
+    # third = randint(1, 254)
+    # fourth = randint(1, 254)
+
+    # ip = '{}.{}.{}.{}'.format(first, second, third, fourth)
+    # return ip
+
+    return ".".join([str(randint(0, 255)) for i in range(4)])
---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+from .utils import yelp_search, get_client_data
+
+class IndexView(View):
+   def get(self, request, *args, **kwargs):
+      items = []
+      city = None
+
+      while not city:
+         res = get_client_data()
+         city = res['city']
+
+      q = request.GET.get('key', None)
+      loc = request.GET.get('loc', None)
+      location = city
+
+      context = {
+         'city': city,
+         'search': False
+      }
+
+      if loc:
+         location = loc
+      if q:
+         items = yelp_search(keyword=q, location=location)
+         context= {
+            'items': items,
+            'city': location,
+            'search': True
+         }
+
+         print(context)
+
+      return render(request, 'index.html', context)
